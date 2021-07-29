8048636e2f12fd1bb4d470089f05fa722ede89c5
butterflysly53 <77926829+butterflysly53@users.noreply.github.com>
2021-07-29 23:00:43 +0530

    Update edit.php

--- edit.php
+++ edit.php
@@ -2,6 +2,7 @@
 <?php 
 $title="index";
 require_once "includes/header.php";
+require_once "includes/auth_check.php";
 require_once "db/conn.php";
 $results=$crud->getSpeciality();
 if(!isset($_GET['id'])){
@@ -15,7 +16,7 @@ $attendees=$crud->getAttendeeDetails($id);
 ?>
 
 <h1 class="text-center">Edit Details</h1>
-<form method="post" action="editpost.php">
+<form method="post" action="editpost.php " >
   <input type="hidden" name='id' value="<?php echo $attendees['attendee_id'] ?>"/>
 
 <div class="mb-3">
@@ -36,20 +37,21 @@ $attendees=$crud->getAttendeeDetails($id);
     </option>
      <?php } ?>
   </select>
-  <div id="Help" class="form-text">Choose any one</div>
+  <div id="Help" class="form-text text-muted">Choose any one</div>
   </div>
  
   <div class="mb-3">
     <label for="email" class="form-label">Email address</label>
     <input type="email" class="form-control" id="email" 
      aria-describedby="emailHelp" placeholder="xyz@gmail.com" name="email" value="<?php echo $attendees['email_id'] ?>">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <div id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</div>
   </div>
+
   <div class="mb-3">
     <label for="phone" class="form-label">Contact Number</label>
     <input type="text" class="form-control" id="phone" aria-describedby="PhoneHelp" name="phone"
      value="<?php echo $attendees['contact_number'] ?>">
-    <div id="PhoneHelp" class="form-text">We'll never share your Phone Number with anyone else.</div>
+    <div id="PhoneHelp" class="form-text text-muted">We'll never share your Phone Number with anyone else.</div>
   </div>
 <a href="viewrecords.php" class="btn btn-primary">Back</a>
 <button type="submit" name="submit" class="btn btn-success">Save Changes</button>
