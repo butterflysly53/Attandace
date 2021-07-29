d6d0c7f1874e6d0378994ed84f0b3e0a870f9df0
butterflysly53 <77926829+butterflysly53@users.noreply.github.com>
2021-07-29 23:02:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $results=$crud->getSpeciality();
 ?>
 
 <h1 class="text-center">Registration for IT Conferenence</h1>
-<form method="post" action="success.php">
+<form  enctype='multipart/form-data' method="post" action="success.php">
 <div class="mb-3">
     <label for="Name" class="form-label"> Your Full Name</label>
     <input required type="text" class="form-control" id="Name" name="Name">
@@ -23,7 +23,7 @@ $results=$crud->getSpeciality();
      <option value="<?php echo $r['speciality_id']?>"> <?php echo $r['Name'];?> </option>
      <?php } ?>
   </select>
-  <div id="Help" class="form-text">Choose any one</div>
+  <div id="Help" class="form-text text-muted">Choose any one</div>
   </div>
  <!--<div class="dropdown">
   <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton1" name="dropdownMenuButton1"
@@ -44,22 +44,28 @@ $results=$crud->getSpeciality();
     <label for="email" class="form-label">Email address</label>
     <input required type="email" class="form-control" id="email" 
      aria-describedby="emailHelp" placeholder="xyz@gmail.com" name="email">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <div id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="phone" class="form-label">Contact Number</label>
     <input required type="text" class="form-control" id="phone" aria-describedby="PhoneHelp" name="phone">
-    <div id="PhoneHelp" class="form-text">We'll never share your Phone Number with anyone else.</div>
+    <div id="PhoneHelp" class="form-text text-muted">We'll never share your Phone Number with anyone else.</div>
   </div>
-  <!--<div class="mb-3">
+  <div class="custom-file">
+            <input type="file" accept="image/*" class="custom-file-input" id="avatar" name="avatar">
+            <label class="custom-file-label" for="avatar">Choose File</label>
+            <small id="avatar" class="form-text text-danger">File Upload is Optional</small>
+            <br/>  
+   </div > 
+  <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label"> Enter Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    <div id="PasswordHelp" class="form-text">It must contain minimum 8 characacters.</div>
+    <div id="PasswordHelp" class="form-text text-muted">It must contain minimum 8 characacters.</div>
   </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>-->
+  </div>
 <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 </form>
 
