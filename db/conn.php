e15959ccca5439d5fde74b5d6d9117cc2c4f9922
butterflysly53 <77926829+butterflysly53@users.noreply.github.com>
2021-07-29 23:09:20 +0530

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,17 +1,17 @@
 <?php
 //Development connection
- $host="127.0.0.1";
- $db="attandance";
- $user="root";
- $pass="";
- $charset="utf8mb4";
-
- //Remote Database Connection
- //$host="remotemysql.com";
- //$db="T1mI7G4qEd";
- //$user="T1mI7G4qEd";
- //$pass="r2g0GlwcxS";
+ //$host="127.0.0.1";
+ //$db="attandance";
+ //$user="root";
+ //$pass="";
  //$charset="utf8mb4";
+
+  Remote Database Connection
+ $host="remotemysql.com";
+ $db="T1mI7G4qEd";
+ $user="T1mI7G4qEd";
+ $pass="r2g0GlwcxS";
+ $charset="utf8mb4";
  $dsn="mysql:host=$host;dbname=$db;charset=$charset";
  
  try{
